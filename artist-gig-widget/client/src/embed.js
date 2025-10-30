f13cc4106c1b46e21b0512d44200c414feb0a3d8
OMCI GitTeam <87039635+OMCI-Source-Code@users.noreply.github.com>
2025-10-30 13:36:01 -0400

    Update embed.js

--- artist-gig-widget/client/src/embed.js
+++ artist-gig-widget/client/src/embed.js
@@ -1,1 +1,11 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import WidgetArtist from "./components/WidgetArtist.jsx";
 
+document.querySelectorAll('[data-gig-widget]').forEach((el) => {
+  const artistId = el.dataset.artistId;
+  const view = el.dataset.view;
+
+  const root = createRoot(el);
+  root.render(<WidgetArtist embedArtistId={artistId} embedView={view} />);
+});
