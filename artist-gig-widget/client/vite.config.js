bf97a69d3b3e35828a983d34b7ab53d099580496
OMCI GitTeam <87039635+OMCI-Source-Code@users.noreply.github.com>
2025-10-30 13:46:46 -0400

    Update vite.config.js

--- artist-gig-widget/client/vite.config.js
+++ artist-gig-widget/client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   build: {
     outDir: 'dist/embed', 
     lib: {
-      entry: resolve(__dirname, 'src/embed.js'), 
+      entry: resolve(__dirname, 'src/embed.jsx'), 
       name: 'ArtistGigWidget',                   
       fileName: 'embed',                         
     },
